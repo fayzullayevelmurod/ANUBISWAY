7c26d3d9c91aa2ef99a2a648c68b01f800dbef4d
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-12-23 16:01:44 +0500

    footer in progress

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-
+$(".error").append("<div class='glitch-window'></div>");
+$( "h1.glitched" ).clone().appendTo( ".glitch-window" );
 // input mask
-let phoneInput = document.getElementById('phone');
-let myForm = document.forms.myForm;
-let result = document.getElementById('result');
+// let phoneInput = document.getElementById('phone');
+// let myForm = document.forms.myForm;
+// let result = document.getElementById('result');
 
-phoneInput.addEventListener('input', function (e) {
-    let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-});
+// phoneInput.addEventListener('input', function (e) {
+//     let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+//     e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+// });
 
-myForm.addEventListener('submit', function(e) {
-  phoneInput.value = phoneInput.value.replace(/\D/g, '');
-  result.innerText = phoneInput.value; 
+// myForm.addEventListener('submit', function(e) {
+//   phoneInput.value = phoneInput.value.replace(/\D/g, '');
+//   result.innerText = phoneInput.value; 
   
-  e.preventDefault();
-});
+//   e.preventDefault();
+// });
 // input mask

2fd6a2bf687b3a0f8eebc6625f7631f6ae03e7fd
Mavlon4658 <mavlon4658@gmail.com>
2024-01-14 14:50:50 +0500

    yangilandi

--- js/main.js
+++ js/main.js
@@ -18,99 +18,108 @@
 // });
 // input mask
 
-// newsSlider
-let swiper_news = new Swiper(".newsSwiper", {
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        // hide: true,
-    },
-    navigation: {
-        nextEl: ".news-button-next",
-        prevEl: ".news-button-prev",
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 24,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 24,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 24,
-        },
-    },
-});
+let home_slider = document.querySelector('.home_slider');
+if (home_slider) {
+  let homeSlider = new Swiper(home_slider, {
+    slidesPerView: 1,
+    mousewheel: true,
+    speed: 800,
+  })
+}
 
 // newsSlider
-let swiper_coperation = new Swiper(".coperationSwiper", {
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        // hide: true,
-    },
-    navigation: {
-        nextEl: ".coperation-button-next",
-        prevEl: ".coperation-button-prev",
-    },
-    breakpoints: {
-        300: {
-          slidesPerView: 2,
-          // spaceBetween: 24,
-        },
-        768: {
-          slidesPerView: 2,
-          // spaceBetween: 24,
-        },
-        1024: {
-          slidesPerView: 6,
-          // spaceBetween: 24,
-        },
-    },
-});
+// let swiper_news = new Swiper(".newsSwiper", {
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//         // hide: true,
+//     },
+//     navigation: {
+//         nextEl: ".news-button-next",
+//         prevEl: ".news-button-prev",
+//     },
+//     breakpoints: {
+//         640: {
+//           slidesPerView: 1,
+//           spaceBetween: 24,
+//         },
+//         768: {
+//           slidesPerView: 2,
+//           spaceBetween: 24,
+//         },
+//         1024: {
+//           slidesPerView: 3,
+//           spaceBetween: 24,
+//         },
+//     },
+// });
 
-// newSlider
-let swiper_new = new Swiper(".newSwiper", {
-    spaceBetween: 24,
-    initialSlide: 1,
-    hashNavigation: {
-      watchState: true,
-    },
-    loop: false,
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        // hide: true,
-    },
-    navigation: {
-        nextEl: ".new-button-next",
-        prevEl: ".new-button-prev",
-    },
-});
+// // newsSlider
+// let swiper_coperation = new Swiper(".coperationSwiper", {
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//         // hide: true,
+//     },
+//     navigation: {
+//         nextEl: ".coperation-button-next",
+//         prevEl: ".coperation-button-prev",
+//     },
+//     breakpoints: {
+//         300: {
+//           slidesPerView: 2,
+//           // spaceBetween: 24,
+//         },
+//         768: {
+//           slidesPerView: 2,
+//           // spaceBetween: 24,
+//         },
+//         1024: {
+//           slidesPerView: 6,
+//           // spaceBetween: 24,
+//         },
+//     },
+// });
+
+// // newSlider
+// let swiper_new = new Swiper(".newSwiper", {
+//     spaceBetween: 24,
+//     initialSlide: 1,
+//     hashNavigation: {
+//       watchState: true,
+//     },
+//     loop: false,
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//         // hide: true,
+//     },
+//     navigation: {
+//         nextEl: ".new-button-next",
+//         prevEl: ".new-button-prev",
+//     },
+// });
 
 
-// newsSlider
-let swiper_donate = new Swiper(".donateSwiper", {
-  scrollbar: {
-      el: ".swiper-scrollbar",
-      // hide: true,
-  },
-  navigation: {
-      nextEl: ".donate-button-next",
-      prevEl: ".donate-button-prev",
-  },
-  breakpoints: {
-      300: {
-        slidesPerView: 2,
-        // spaceBetween: 24,
-      },
-      768: {
-        slidesPerView: 2,
-        // spaceBetween: 24,
-      },
-      1024: {
-        slidesPerView: 5,
-        // spaceBetween: 24,
-      },
-  },
-});
+// // newsSlider
+// let swiper_donate = new Swiper(".donateSwiper", {
+//   scrollbar: {
+//       el: ".swiper-scrollbar",
+//       // hide: true,
+//   },
+//   navigation: {
+//       nextEl: ".donate-button-next",
+//       prevEl: ".donate-button-prev",
+//   },
+//   breakpoints: {
+//       300: {
+//         slidesPerView: 2,
+//         // spaceBetween: 24,
+//       },
+//       768: {
+//         slidesPerView: 2,
+//         // spaceBetween: 24,
+//       },
+//       1024: {
+//         slidesPerView: 5,
+//         // spaceBetween: 24,
+//       },
+//   },
+// });
